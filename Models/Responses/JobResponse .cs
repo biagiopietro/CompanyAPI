41a68ba78cec5b08ed47bc5739f439f9f34e14a2
MrJack <biagio.toby@hotmail.it>
2020-06-10 15:19:24 +0200

    Removed Constructor in JobResponse

--- Models/Responses/JobResponse .cs
+++ Models/Responses/JobResponse .cs
@@ -4,11 +4,6 @@ namespace Responses
 {
     public class JobResponse
     {
-        public JobResponse(long id, string name)
-        {
-            Id = id;
-            Name = name;
-        }
         public long Id { get; set; }
         public string Name {get; set;}
     }
